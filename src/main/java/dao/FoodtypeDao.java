a6a53244f11db0103447c5ae1397645c58f2173d
George Okumu <gokumu12@gmail.com>
2020-09-25 06:49:24 +0300

    implemented many-to-many table relationship since our restaurant will have to list foodtypes it serves  and our foodtypes will have which restaurant serves it

--- src/main/java/dao/FoodtypeDao.java
+++ src/main/java/dao/FoodtypeDao.java
@@ -5,10 +5,18 @@ import models.Foodtype;
 import java.util.List;
 
 public interface FoodtypeDao {
+    //CREATE methods
     void add(Foodtype foodtype);
+
+  //updating methods
     //void update();
+
+    //Read methods
     List<Foodtype> getAll();
+
     //void findById(Foodtype foodtype);
+
     void deleteById(int id);
+
     void clearAll();
 }
